e31d1c84e8b2b6b2163856e507c69cb5a4c1a1dc
sh4869 <nobuk4869@gmail.com>
2022-07-08 00:55:59 +0900

    indexの削除

--- sql/2_add_index.sql
+++ sql/2_add_index.sql
@@ -1,2 +1,2 @@
-create index jia_isu_uuid_index on isu_condition(jia_isu_uuid DESC);
-create index jia_isu_uuid_and_timpestamp__index on isu_condition(jia_isu_uuid, timestamp DESC);
+-- create index jia_isu_uuid_index on isu_condition(jia_isu_uuid DESC);
+-- create index jia_isu_uuid_and_timpestamp__index on isu_condition(jia_isu_uuid, timestamp DESC);
